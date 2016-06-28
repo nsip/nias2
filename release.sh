9737ee88d6f65949fe555cde9adbceba003f1531
Scott Penrose <scottp@dd.com.au>
2016-06-28 16:22:13 +1000

    Release script update

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 
-cd release; go build; cd ..
-./release/release silis go-nias-Mac.zip build/go-nias-Mac.zip
-./release/release silis go-nias-Win64.zip build/go-nias-Win64.zip
-./release/release silis go-nias-Win32.zip build/go-nias-Win32.zip
-./release/release silis go-nias-Linux64.zip build/go-nias-Linux64.zip
-./release/release silis go-nias-Linux32.zip build/go-nias-Linux32.zip
-./release/release silis go-nias-LinuxArm7.zip build/go-nias-LinuxArm7.zip
+cd tools; go build release.go; cd ..
+./tools/release nias2 go-nias-Mac.zip build/go-nias-Mac.zip
+./tools/release nias2 go-nias-Win64.zip build/go-nias-Win64.zip
+./tools/release nias2 go-nias-Win32.zip build/go-nias-Win32.zip
+./tools/release nias2 go-nias-Linux64.zip build/go-nias-Linux64.zip
+./tools/release nias2 go-nias-Linux32.zip build/go-nias-Linux32.zip
+./tools/release nias2 go-nias-LinuxArm7.zip build/go-nias-LinuxArm7.zip
