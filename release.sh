42b7c55b899a9bbe967dd60e6e6bc5221e376f19
Scott Penrose <scottp@dd.com.au>
2016-06-28 16:40:08 +1000

    Remove ARM release

--- release.sh
+++ release.sh
@@ -5,4 +5,3 @@ cd tools; go build release.go; cd ..
 ./tools/release nias2 go-nias-Win32.zip build/go-nias-Win32.zip
 ./tools/release nias2 go-nias-Linux64.zip build/go-nias-Linux64.zip
 ./tools/release nias2 go-nias-Linux32.zip build/go-nias-Linux32.zip
-./tools/release nias2 go-nias-LinuxArm7.zip build/go-nias-LinuxArm7.zip
