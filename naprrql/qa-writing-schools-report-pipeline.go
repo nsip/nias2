81727a1aa05590240efa6de7c67aa1a855de3bdc
Nick Nicholas <opoudjis@optusnet.com.au>
2019-03-13 12:30:50 +1100

    Support for future F participation code

--- naprrql/qa-writing-schools-report-pipeline.go
+++ naprrql/qa-writing-schools-report-pipeline.go
@@ -260,7 +260,7 @@ func qaWritingAttemptsQueryExecutor(ctx context.Context, query string, url strin
 			for _, result := range json.Array() {
 				participation := result.Get("Event.ParticipationCode").String()
 				domain := result.Get("Test.TestContent.TestDomain").String()
-				if domain == "Writing" && participation == "P" {
+				if domain == "Writing" && (participation == "P" || participation == "F") {
 					key := result.Get("Test.TestContent.TestLevel").String() + ":" + domain
 					school.TestAttempts[key]++
 					school.TotalAttempts++
