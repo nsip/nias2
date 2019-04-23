adf907df8bf0a4dc4683f9a332282c13939b8a30
Scott Penrose <scottp@dd.com.au>
2019-04-23 17:27:20 +1000

    Build for naprralhp - not complete

---
+++ build_naprrqlhp.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -e
+
+CWD=`pwd`
+
+
+do_build() {
+	echo "Building NAPRRQLHP..."
+	mkdir -p $OUTPUT
+	cd $CWD
+	cd ./app/naprrqlhp
+	go get
+	GOOS="$GOOS" GOARCH="$GOARCH" go build -ldflags="$LDFLAGS" -o $OUTPUT/$HARNESS
+}
+
+
+
+
+      do_zip() {
+        cd $OUTPUT
+        cd ..
+        zip -qr ../$ZIP naprrqlhp
+        cd $CWD
+      }
+
+      build_mac64() {
+        # MAC OS X (64 only)
+        echo "Building Mac binaries..."
+        GOOS=darwin
+        GOARCH=amd64
+        LDFLAGS="-s -w"
+        OUTPUT=$CWD/build/Mac/naprrqlhp
+        # GNATS=nats-streaming-server
+        HARNESS=naprrqlhp
+        AUDITDIFFHARNESS=napcomp
+        ZIP=naprrqlhp-Mac.zip
+        do_build
+        #do_upx
+        # do_shells
+        # do_zip
+        echo "...all Mac binaries built..."
+      }
+
+
+      build_windows64() {
+        # WINDOWS 64
+        echo "Building Windows64 binaries..."
+        GOOS=windows
+        GOARCH=amd64
+        LDFLAGS="-s -w"
+        OUTPUT=$CWD/build/Win64/naprrqlhp
+        # GNATS=nats-streaming-server.exe
+        HARNESS=naprrqlhp.exe
+        AUDITDIFFHARNESS=napcomp.exe
+        ZIP=naprrqlhp-Win64.zip
+        do_build
+        #do_upx
+        # do_bats
+        # do_zip
+        echo "...all Windows64 binaries built..."
+      }
+
+      build_linux64() {
+        # LINUX 64
+        echo "Building Linux64 binaries..."
+        GOOS=linux
+        GOARCH=amd64
+        LDFLAGS="-s -w"
+        OUTPUT=$CWD/build/Linux64/naprrqlhp
+        # GNATS=nats-streaming-server
+        HARNESS=naprrqlhp
+        AUDITDIFFHARNESS=napcomp
+        ZIP=naprrqlhp-Linux64.zip
+        do_build
+        #do_goupx
+        # do_shells
+        # do_zip
+        echo "...all Linux64 binaries built..."
+      }
+
+      # TODO ARM
+      # GOOS=linux GOARCH=arm GOARM=7 go build -o $CWD/build/LinuxArm7/go-nias/aggregator
+
+      if [ "$1" = "L64"  ]
+      then
+        build_linux64
+      elif [ "$1" = "W64"  ]
+      then
+        build_windows64
+      elif [ "$1" = "M64"  ]
+      then
+        build_mac64
+      else
+        build_mac64
+        build_windows64
+        build_linux64
+      fi
