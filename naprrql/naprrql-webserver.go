ab0f52d089da3d2e0dc6a834f80cc46aadf57fd0
Nick Nicholas <opoudjis@gmail.com>
2021-08-18 14:09:28 +1000

    security update: upgrade echo: https://github.com/nsip/nias2/issues/108

--- naprrql/naprrql-webserver.go
+++ naprrql/naprrql-webserver.go
@@ -7,8 +7,8 @@ package naprrql
 import (
 	"net/http"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/playlyfe/go-graphql"
 )
 
